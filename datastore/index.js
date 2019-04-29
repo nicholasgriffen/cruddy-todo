49cac16c9ba48c9c0e386d2f7b9d90102c3badd7
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-04-29 16:16:50 -0600

    promisify readOne, update

--- datastore/index.js
+++ datastore/index.js
@@ -36,48 +36,52 @@ exports.readAll = (callback) => {
     if (err) {
       callback(err);
     } else if (!files) {
-      callback(null, [])
+      callback(null, []);
     } else {
       Promise.all(_.map(files, (fileName) => {
-        var id = fileName.split('.txt')[0]
+        var id = fileName.split('.txt')[0];
         return new Promise((resolve, reject) => {
           fs.readFile(todoFilePath(id), (err, todoBuff) => {
             if (err) {
-              reject(err)
+              reject(err);
             } else {
-              resolve({ id, text: String(todoBuff) })
+              resolve({ id, text: String(todoBuff) });
             }
           })
         })
-      })).then(todos => callback(null, todos))
+      })).then(todos => callback(null, todos)).catch(e => callback(e))
     }
   })
 };
 
 exports.readOne = (id, callback) => {
-  fs.readFile(todoFilePath(id), (err, buff) => {
-    if (err) {
-      callback(new Error(`No item with id: ${id}`));
-    } else {
-      callback(null, { id, text: String(buff) });
-    }
-  })
+  return new Promise((resolve, reject) => {
+    fs.readFile(todoFilePath(id), (err, buff) => {
+      if (err) {
+        reject(new Error(`No item with id: ${id}`));
+      } else {
+        resolve({ id, text: String(buff) });
+      }
+    })
+  }).then(todo => callback(null, todo)).catch(e => callback(e));
 };
 
 exports.update = (id, text, callback) => {
-  exports.readOne(id, (err, _todo) => {
-    if (err) {
-      callback(err);
-    } else {
-      fs.writeFile(todoFilePath(id), text, (err) => {
-        if (err) {
-          callback(err)
-        } else {
-          callback(null, { id, text });
-        }
-      })
-    }
-  })
+  return new Promise((resolve, reject) => {
+    exports.readOne(id, (err, _todo) => {
+      if (err) {
+        reject(err);
+      } else {
+        fs.writeFile(todoFilePath(id), text, (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve({ id, text });
+          }
+        })
+      }
+    })
+  }).then(todo => callback(null, todo)).catch(e => callback(e))
 };
 
 exports.delete = (id, callback) => {
