8e4ba1870e98284a3e713a90041586ba9de653ac
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-04-29 12:01:44 -0600

    passes basic spec for delete

--- datastore/index.js
+++ datastore/index.js
@@ -46,7 +46,7 @@ exports.readOne = (id, callback) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
-      callback(null, { id, text: String(buff) })
+      callback(null, { id, text: String(buff) });
     }
   })
 };
@@ -68,14 +68,19 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  exports.readOne(id, (err, _todo) => {
+    if (err) {
+      callback(err);
+    } else {
+      fs.unlink(path.join(exports.dataDir, id + '.txt'), (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback();
+        }
+      })
+    }
+  })
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
